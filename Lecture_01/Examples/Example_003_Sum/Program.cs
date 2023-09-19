4b6924b3be06930b190dc46b2f589560af026867
Ruslan <izya.man18@gmail.com>
2023-09-19 20:38:20 +0300

    add examples from 1 lecture

--- Lecture_01/Examples/Example_003_Sum/Program.cs
+++ Lecture_01/Examples/Example_003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int num1 = 31;
-int num2 = 55;
+﻿int num1 = new Random().Next(1,10);
+Console.WriteLine(num1);
+int num2 = new Random().Next(1,10);
+Console.WriteLine(num2);
 Console.WriteLine(num1 + num2);
