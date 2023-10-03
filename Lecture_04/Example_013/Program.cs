62051ed0dcd9d8f606123a2c5896eceede8bed88
Ruslan <izya.man18@gmail.com>
2023-10-03 16:49:02 +0300

    add lecture 04 and homework from seminar 05

---
+++ Lecture_04/Example_013/Program.cs
@@ -0,0 +1,138 @@
+﻿// Двумерные массивы
+
+string[,] table = new string[2, 5];
+table[1, 2] = "слово";
+
+
+
+for (int rows = 0; rows < 2; ++rows)
+{
+    for (int cols = 0; cols < 5; ++cols)
+    {
+        Console.Write($"-{table[rows, cols]}-");
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); ++rows)
+    {
+        for (int cols = 0; cols < matrix.GetLength(1); ++cols)
+        {
+            Console.Write($"{matrix[rows, cols]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); ++rows)
+    {
+        for (int cols = 0; cols < matrix.GetLength(1); ++cols)
+        {
+            matrix[rows, cols] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+void PrintImage(int[,] pic)
+{
+    for (int rows = 0; rows < pic.GetLength(0); ++rows)
+    {
+        for (int cols = 0; cols < pic.GetLength(1); ++cols)
+        {
+            if (pic[rows, cols] == 1)
+                Console.Write("+");
+            else
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Закрашивание рисунка как пример рекурсивной функции
+
+int[,] pic = new int[,]
+{
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+Console.WriteLine();
+PrintImage(pic);
+FillImage(13, 13);
+Console.WriteLine();
+PrintImage(pic);
+
+// Факториал
+
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 1; i < 40; ++i)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+// Фибоначчи 
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n - 1) + f(n - 2)
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 50; ++i)
+{
+    Console.WriteLine($"f{i} - {Fibonacci(i)}");
+}
---
+++ Seminar_05/HomeWork/Program.cs
@@ -0,0 +1,111 @@
+﻿// Домашнее задание по семинару 5
+
+Console.WriteLine("Добрый день! Домашнее задание Измайлова Р.Э. по 5 семинару курса Введение в С#");
+Console.Write("Введите номер задачи из перечисленных (34, 36, 38): ");
+int numTask = Convert.ToInt32(Console.ReadLine());
+while(numTask != 34 && numTask != 36 && numTask != 38)
+{ 
+    Console.Write("Введите корректный номер задачи из перечисленных (34, 36, 38): ");
+    numTask = Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; ++i)
+    {
+        arr[i] = rnd.Next(-100, 100);
+    }
+}
+
+void FillArray3(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; ++i)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+}
+
+int CntEvenNums(int[] arr)
+{
+    int cnt = 0;
+    for (int i = 0; i < arr.Length; ++i)
+    {
+        if (arr[i] % 2 == 0) cnt++;
+    }
+    return cnt;
+}
+
+int SumNotEvenIndexNums(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+double FindMax(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 1; i < arr.Length; ++i)
+    {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+double FindMin(double[] arr)
+{
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; ++i)
+    {
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+
+Console.WriteLine($"Вы выбрали номер задачи - {numTask}");
+switch(numTask)
+{
+    case 34:
+    {
+        Console.WriteLine("Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.");
+        Console.Write("Введите целое число, кол-во элементов в массиве: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int [size];
+        FillArray3(arr);
+        Console.WriteLine(string.Join(", ", arr));
+        Console.WriteLine($"Количество четных чисел - {CntEvenNums(arr)}");
+        break;
+    }
+    case 36:
+    {
+        Console.WriteLine("Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
+        Console.Write("Введите целое число, кол-во элементов в массиве: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int [size];
+        FillArray(arr);
+        Console.WriteLine(string.Join(", ", arr));
+        Console.WriteLine($"Сумма элементов на нечетных индексах - {SumNotEvenIndexNums(arr)}");
+        break;
+    }
+    case 38:
+    {
+        Console.WriteLine("Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.");
+        Console.Write("Введите целое число, кол-во элементов в массиве: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        double[] arr = new double [size];
+        Console.Write("Введите числа массива каждое на новой строке: ");
+        for (int i = 0; i < arr.Length; ++i)
+        {
+            arr[i] = Convert.ToDouble(Console.ReadLine());
+        }
+        Console.WriteLine(string.Join(", ", arr));
+        double max = FindMax(arr);
+        double min = FindMin(arr);
+        Console.WriteLine($"Разница между максимальным и минимальным элементами - {max - min}");
+        break;
+    }
+}
