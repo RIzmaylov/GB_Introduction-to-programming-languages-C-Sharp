6056f2ad733322aaa147a5499f904f63cf161e52
Ruslan <izya.man18@gmail.com>
2023-10-16 22:32:30 +0300

    add seminar09

---
+++ Seminar_09/Task65/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+void RoadToNWithM(int M, int N)
+{
+    if (M > N) return;
+    RoadToNWithM(M, N - 1);
+    Console.Write($"{N} ");
+}
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+RoadToNWithM(M, N);
---
+++ Seminar_09/Task67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 67: Напишите программу, 
+//которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumDigital(int N)
+{
+    if (N == 0) return 0;
+    return N % 10 + SumDigital(N / 10);
+}
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа {N} = {SumDigital(N)}");
